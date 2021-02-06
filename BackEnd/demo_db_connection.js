c7e94e4e977931c055ab037f05f462c7aac94822
srinivas nayak <srinivasnayak@macbook-air.home>
2021-02-06 07:55:16 +0100

    Backend

---
+++ BackEnd/demo_db_connection.js
@@ -0,0 +1,36 @@
+var mysql = require('mysql');
+
+
+
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database:"Lottery"
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+});
+
+
+
+
+  // var sql = "INSERT INTO players (id,player,chance1,chance2,chance3,chance4,chance5) VALUES (5, 'player6',1,2,3,4,5)";
+  // db.query(sql, function (err, result) {
+  //  if (err) throw err;
+  //  console.log("1 record inserted");
+  // });
+
+
+  db.query("SELECT * FROM players", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+});
+
+
+
+
+
---
+++ BackEnd/server.js
@@ -0,0 +1,35 @@
+//I want to give my Own Input to Database
+//
+
+
+// To be completed
+// 1) To query string with our own hardcoded value
+// 2) To retrevie the Whole table from the Database
+
+// Push request - 1) To query string with our own hardcoded value
+//Get request -  2) To retrevie the Whole table from the Database
+
+// How to
+
+// How to connect both Node js and Angular JS
+
+// 1)Pass Data to front end 
+// 2) How to get values from the Front ENd
+
+
+
+
+
+const express=require('express');
+
+const app=express(),
+	  bodyParser=require("body-parser");
+	  port=3080;
+
+app.listen(3080,()=>{
+	console.log('server is listening in port 3080')
+});
+
+
+
+
